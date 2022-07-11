bb9b6916fdf60e9d354ecccbb120f340ea9c7ffb
LitvinVK <LitvinVK@yandex.ru>
2022-07-11 18:50:10 +0300

    task 2.30

---
+++ task 2.30/js/script.js
@@ -0,0 +1,49 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const ads = document.querySelectorAll('.promo__adv > img');
+ads.forEach(item => {
+    item.remove();
+});
+
+const genre = document.querySelector('.promo__genre');
+genre.textContent = 'ДРАМА';
+
+const bg = document.querySelector('.promo__bg');
+bg.style.backgroundImage = 'url("img/bg.jpg")';
+
+const movies = document.querySelector('.promo__interactive-list');
+movies.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movies.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}: ${film}
+            <div class="delete"></div>
+        </li>
+    `
+});
